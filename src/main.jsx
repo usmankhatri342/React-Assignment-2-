0845133df5a5d57a104fb3c0221973cc35e4dcd7
usmanramzan342 <usmantt.ramzan@gmail.com>
2024-10-23 14:29:22 +0500

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,75 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <>
-    <div class="intro">
-      <h1>Welcome to my website!</h1>
-    </div>
-    <p class="summary">
-      You can find my thoughts here.
-      <br></br>
-      <b>And</b> <i>pictures</i> of scientists!
-    </p>
-    </>
-  </StrictMode>,
-)
+   createRoot(document.getElementById('root')).render(
+       <>
+       <div class="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p class="summary">
+        You can find my thoughts here.
+        <br/><br/>
+        <b>And <i>pictures</i></b> of scientists!
+      </p>
+      </>
+    );
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+  //   <>
+  //   <div class="intro">
+  //     <h1>Welcome to my website!</h1>
+  //   </div>
+  //   <p class="summary">
+  //     You can find my thoughts here.
+  //     <br></br>
+  //     <b>And</b> <i>pictures</i> of scientists!
+  //   </p>
+  //   </>
+  // </StrictMode>,
+  // );
